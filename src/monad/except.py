a4ee32f523cf7c4834ad97f7e6b43a70b81f7aaf
yongqian <yongqian@live.com.my>
2024-04-29 10:02:34 -0700

    add exception monad

--- src/monad/except.py
+++ src/monad/except.py
@@ -33,5 +33,27 @@ def fail(fb):
 def runExcept(f: Except) -> Either:
     return f.match(EXCEPT=lambda a: a)
 
+def liftExcept(f: Callable[[Any], Any], m: Except) -> Except:
+    return monad(m, lambda a: _return(m, f(a)))
+
 ###################################################################
 # operations
+
+def throwE(e: E) -> Except:
+    return Except.EXCEPT(Either.LEFT(e))
+
+def catchE(exp: Except, f: Callable[[E], Except]) -> Except:
+    return Except.EXCEPT(runExcept(exp).match(
+            left=lambda l: runExcept(f(l)),
+            right=lambda r: Either.RIGHT(r)))
+
+# similar to catchE, just we never bail out
+def tryE(exp: Except) -> Except[E, Either]:
+    return catchE(liftExcept(lambda a: Either.RIGHT(a), exp),
+                  lambda l:  Except.EXCEPT(Either.RIGHT(Either.LEFT(l))))
+
+def finallyE(exp: Except, closer: Except) -> Except:
+    return monad(tryE(exp), lambda res:
+                 monad(closer, lambda a: res.match(
+                     right=lambda r: Except.EXCEPT(Either.RIGHT(r)),
+                     left=lambda l: throwE(l))))
